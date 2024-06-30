a185fb23db60fc39e163b14d1a10fed87e27b0d1
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-06-30 17:17:42 +0300

    finished calculator

--- script.js
+++ script.js
@@ -1,5 +1,79 @@
-console.log("hello world");
+let firstValue = 0;
+let secondValue = 0;
+let operator 
 
+const displayValues = [];
+
+const display = document.querySelector("span"); 
+const numberButtons = document.querySelectorAll("button.number");
+const clearButton = document.querySelector("button.clear-button");
+const equalButton = document.querySelector("button.equal-button");
+const operators = document.querySelectorAll("button.operator");
+const dotButton = document.querySelector("button.dot-button");
+
+dotButton.addEventListener("click", () => {
+    const checkForDot = displayValues.filter((element) => element == "." ) 
+    if(checkForDot.length == 0){ 
+    displayValues.push(dotButton.textContent);
+    display.textContent = displayValues.join("");
+    };
+});
+
+numberButtons.forEach((button) => button.addEventListener("click",() =>{
+    displayValues.push(button.textContent);
+    display.textContent = displayValues.join("");
+}));
+
+operators.forEach((button) => button.addEventListener("click", () => {
+    if(firstValue === 0 && display.textContent != "Error"){
+    firstValue = parseFloat(display.textContent); 
+    } 
+    displayValues.length = 0;
+    operator = button.textContent;
+}));
+
+clearButton.addEventListener("click", () => {
+    firstValue = 0;
+    secondValue = 0;
+    displayValues.length = 0;
+    display.textContent = 0;
+});
+
+equalButton.addEventListener("click", () => {
+    if(secondValue == 0){
+    secondValue = parseFloat(display.textContent);
+    };
+    if(secondValue == 0 && operator == "/"){
+        display.textContent = "Error";
+        firstValue = 0;
+        secondValue = 0;
+        displayValues.length = 0;
+    }else{
+    const result = operate(firstValue,secondValue,operator);
+    if(result != Math.floor(result)){
+        const twoDecimalsResult = (Math.round(result * 100) / 100).toFixed(2);
+        display.textContent = twoDecimalsResult; 
+        firstValue = parseFloat(twoDecimalsResult);
+    }else{
+        display.textContent = result;
+        firstValue = parseFloat(result);
+    }    
+    }
+});
+
+function operate(a,b,op){
+    if(op === "+"){
+        return add(a,b);
+    }else if(op === "-"){
+        return subtract(a,b);
+    }else if(op === "*"){
+        return multiply(a,b);
+    }else if(op === "/"){
+        return divide(a,b);
+    }else{
+        return b
+    }
+};
 
 function add(a,b){
     return a + b;
@@ -19,7 +93,3 @@ function divide(a,b){
 };
 
 
-console.log(add(10,2))
-console.log(subtract(10,2));
-console.log(multiply(10,2));
-console.log(divide(10,2));
